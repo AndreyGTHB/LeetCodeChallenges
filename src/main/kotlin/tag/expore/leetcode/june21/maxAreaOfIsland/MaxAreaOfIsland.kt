95ec8f1887e609fb3336c2979a8fe0cdc4916e87
AndreyGTHB <andr.g.tuk@gmail.com>
2021-06-29 14:03:40 +0700

    1 task added

---
+++ src/main/kotlin/tag/expore/leetcode/june21/maxAreaOfIsland/MaxAreaOfIsland.kt
@@ -0,0 +1,14 @@
+package tag.expore.leetcode.june21.maxAreaOfIsland
+
+fun main() {
+    val sea = arrayOf(
+        intArrayOf(1, 1, 0, 0, 0),
+        intArrayOf(0, 1, 0, 1, 1),
+        intArrayOf(1, 0, 0, 1, 1),
+    )
+
+}
+
+fun processCell() {
+
+}
